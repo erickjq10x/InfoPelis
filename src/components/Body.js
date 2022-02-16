e16edd695ccdb3a240839650540215bc9b65e973
Erick Quispe <erickjoel10x@gmail.com>
2022-02-16 14:54:13 -0300

    upload repository

---
+++ src/components/Body.js
@@ -0,0 +1,4 @@
+const Body = () =>{
+
+}
+export default Body
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-container">
+                <p className="header__title">InfoPelis</p>
+            </div>
+        </header>
+    )
+}
+export default Header
